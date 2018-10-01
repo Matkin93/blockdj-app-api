32b7e14b6b7e54826aee508b20640d347993afd8
Kingsley Onyensoh <King@Kingsleys-MacBook-Pro.local>
2018-10-01 12:01:24 +0100

    implement City model

--- models/Comment.js
+++ models/Comment.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose')
-const { Schema } = mongoose
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const CommentSchema = new Schema({
   profile: {
@@ -18,6 +18,6 @@ const CommentSchema = new Schema({
   updated_at: {
     type: Date
   }
-})
+});
 
 module.exports = mongoose.model('comments', CommentSchema);
