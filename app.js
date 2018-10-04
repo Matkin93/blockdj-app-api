08561f1a700c32b8adadc02a6a442fc9bd0fd1af
Kingsley Onyensoh <King@Kingsleys-MacBook-Pro.local>
2018-10-04 11:16:01 +0100

    implement error handling block

--- app.js
+++ app.js
@@ -10,6 +10,25 @@ app.use(cors())
 app.use(bodyparser)
 
 app.use('/api', apiRouter)
+app.use('/*', (req, res, next) => {
+  next({status: 400, msg: 'page not found'})
+})
+
+app.use((err, req, res, next) => {
+  if (err.name === 'CastError' || err.name === 'ValidationError') {
+      err.status = 400
+      if (!err.msg) err.msg = 'Bad Request'     
+  } 
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  if (err.status !== 404 && err.status !== 400) {
+      err.status = 500
+      err.msg = 'Something has gone horribly wrong'
+  }
+  res.status(err.status).send({msg: err.msg})
+})
 
 mongoose.connect(DB_URL, {useNewUrlParser: true})
 .then(() => {
